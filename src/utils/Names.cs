b535df8c17910b2eb1daa2af9a3cdd34e154553e
Claudiu Comsa <comshak@gmail.com>
2007-10-28 23:35:52 +0000

    Removed a double backslash problem.

--- src/utils/Names.cs
+++ src/utils/Names.cs
@@ -4,8 +4,8 @@ namespace com.comshak.FeedReader
 {
 	internal class Names
 	{
-		public const string FeedsFolder = @"feeds\\";
-		public const string TempFolder = @"temp\\";
+		public const string FeedsFolder = @"feeds\";
+		public const string TempFolder = @"temp\";
 
 		private Names()
 		{
